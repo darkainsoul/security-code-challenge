e08b4839c111dbd9c61f2b1790be130e9947a29f
Luis Segura <darkainsoul@gmail.com>
2023-11-14 00:24:19 -0600

    Security Fixes

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require("express");
+const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const { Console } = require("console");
 require("dotenv").config();
 const rateLimit = require("express-rate-limit");
 const Joi = require("joi");
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutos
   max: 100, // Número máximo de solicitudes por ventana
 });
 
-const schema = Joi.object({
-  amount: Joi.number().required(),
-});
-
 // Create an instance of Express.js
 const app = express();
 
+// Use Helmet!
+app.use(helmet());
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
@@ -24,12 +24,6 @@ app.use(express.urlencoded({ extended: false }));
 
 // Initialize user account balance
 let accountBalance = 0;
-
-app.get("/login", (req, res) => {
-  res.send(
-    '<html><head></head><form method="post" action="/auth"><input type="button" value="Login"></form><body></body></html>'
-  );
-});
 
 app.post("/auth", (req, res) => {
   // Get the data from the Form or any input if thats exists
@@ -63,6 +57,13 @@ function validateToken(req, res, next) {
     }
   });
 }
+function validateInput(data)
+{
+  const schema = Joi.object({
+    amount: Joi.number().required(),
+  }).options({ abortEarly: false });
+  return schema.validate(data)
+}
 
 app.get("/balance", limiter, validateToken, (req, res) => {
   res.json({ balance: accountBalance });
@@ -70,10 +71,10 @@ app.get("/balance", limiter, validateToken, (req, res) => {
 
 // Endpoint to make a deposit
 app.post("/deposit", limiter, validateToken, (req, res) => {
-  console.log("req:", req.body);
-  const { amount } = schema.validate(req.body);
+  response = validateInput(req.body)
+  const { amount } = response.value;
 
-  if (!amount || isNaN(amount) || amount <= 0) {
+  if (response.error || amount <= 0) {
     return res.status(400).json({ error: "Invalid amount" });
   }
 
@@ -83,9 +84,11 @@ app.post("/deposit", limiter, validateToken, (req, res) => {
 
 // Endpoint to process a withdrawal
 app.post("/withdraw", limiter, validateToken, (req, res) => {
-  const { amount } = req.body;
 
-  if (!amount || isNaN(amount) || amount <= 0) {
+  response = validateInput(req.body)
+  const { amount } = response.value;
+
+  if (response.error || amount <= 0) {
     return res.status(400).json({ error: "Invalid amount" });
   }
 
